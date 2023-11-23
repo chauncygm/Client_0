86d2ee592c3a759b5032bd507fc81fa8b3d1a129
gsj <chauncygm@gmail.com>
2023-11-23 20:42:47 +0800

    navmash自动寻路测试scene

---
+++ Assets/Scripts/AutoPathFinding.cs
@@ -0,0 +1,39 @@
+﻿//AutoPathFinding.cs 挂在角色的gameobject上
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AutoPathFinding : MonoBehaviour
+{
+    private NavMeshAgent m_navAgent;
+    private Transform m_selfTrans;
+    private Vector3 m_targetPos;
+    private bool m_isMoving = false;
+    
+    void Awake()
+    {
+        // 给角色挂上NavMeshAgent组件
+        m_navAgent = gameObject.GetComponent<NavMeshAgent>();
+        m_selfTrans = transform;
+    }
+    
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                m_targetPos = hit.point;
+                // 自动寻路移动到目标点
+                m_navAgent.SetDestination(m_targetPos);
+                m_isMoving = true;
+            }
+        }
+        
+        if(Vector3.Distance(m_selfTrans.position, m_targetPos) <= 0.1f)
+        {
+            m_isMoving = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SlowMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMove : MonoBehaviour
+{
+    private Transform start;
+
+    public Transform end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = GameObject.Find("OneCube").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(start.position, end.position, Time.deltaTime);
+    }
+}
