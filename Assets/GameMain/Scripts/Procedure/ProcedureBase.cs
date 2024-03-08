bc557abd79644a385b797a93f3181fd7386dff80
gsj <chauncygm@gmail.com>
2024-03-08 15:16:42 +0800

    stash commit

---
+++ Assets/GameMain/Scripts/Procedure/ProcedureBase.cs
@@ -0,0 +1,9 @@
+﻿namespace GameMain.Scripts.Procedure
+{
+    public abstract class ProcedureBase : GameFramework.Procedure.ProcedureBase
+    {
+    
+        // 基础功能TODO
+    
+    }
+}
--- Assets/GameMain/Scripts/Procedure/ProcedureMain.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureMain.cs
@@ -1,21 +1,25 @@
-﻿using GameFramework;
-using GameFramework.Procedure;
+﻿using Example;
+using GameFramework;
 using UnityEngine;
 using UnityGameFramework.Runtime;
 using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
 
-namespace Example {
-    public class ProcedureExample : ProcedureBase
+namespace GameMain.Scripts.Procedure
+{
+    public class ProcedureMain : ProcedureBase
     {
+
         protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
-
-            var welcomeMessage = Utility.Text.Format("Hello! This is an empty project based on Game Framework {0}.", Version.GameFrameworkVersion);
+            
+            var welcomeMessage = Utility.Text.Format("Hellooo! This is an empty project based on Game Framework {0}.", Version.GameFrameworkVersion);
             Log.Info(welcomeMessage);
             Log.Warning(welcomeMessage);
             Log.Error(welcomeMessage);
             Debug.Log(welcomeMessage);
+            ChangeState<ProcedureExample>(procedureOwner);
+            
         }
     }
 }
