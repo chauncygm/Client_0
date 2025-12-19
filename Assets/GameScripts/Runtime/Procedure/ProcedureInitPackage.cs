63974ac3c6ca77f79e386d7fa7d187351dfb8b8e
gsj <704311761@qq.com>
2025-12-19 14:08:36 +0800

    修改默认package名字

--- Assets/GameScripts/Runtime/Procedure/ProcedureInitPackage.cs
+++ Assets/GameScripts/Runtime/Procedure/ProcedureInitPackage.cs
@@ -15,6 +15,7 @@ namespace GameMain
         protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
+            GameModule.Resource.PackageName = "GamePackage";
             
             //Fire Forget立刻触发UniTask初始化Package
             InitPackage(procedureOwner).Forget();
